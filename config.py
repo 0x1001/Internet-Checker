6f3ff9663b509365bd0db2ca6dda4ff4c0413caf
Damian Nowok <damian.nowok@gmail.com>
2013-10-18 23:00:02 +0200

    Adding also web checker.
    Im happy with this version for now.

---
+++ config.py
@@ -0,0 +1,6 @@
+MODEM_URL_ADDRESS = 'http://192.168.1.1'
+MODEM_URL_STATUS_ADDRESS = 'http://192.168.1.1/status/status_deviceinfo.htm'
+MODEM_USERNAME = 'admin'
+MODEM_PASSWORD = 'admin'
+MODEM_REGEXP_INTERNET_ACTIVE = 'Showtime'
+WEBCHECK_PAGE_LIST = ['http://google.com','http://www.python.org','http://github.com','http://www.amazon.com','http://www.nasa.gov','http://lhcathome.web.cern.ch']
--- internet_check.py
+++ internet_check.py
@@ -1,61 +1,94 @@
+################################################################################
+########################## Modem Analyzer ######################################
+################################################################################
 def getModemPage():
     """
-        This function connects to modem internal web pages and 
+        This function connects to modem internal web pages and
         gets status information
-        
+
         Input:
         Nothing
-        
+
         Returns:
         Pages string
     """
     import urllib2
-
-    theurl = 'http://192.168.1.1'
-    username = 'admin'
-    password = 'admin'
+    import config
 
     passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    passman.add_password(None, theurl, username, password)
+    passman.add_password(None,config.MODEM_URL_ADDRESS,config.MODEM_USERNAME,config.MODEM_PASSWORD)
     authhandler = urllib2.HTTPBasicAuthHandler(passman)
     opener = urllib2.build_opener(authhandler)
     urllib2.install_opener(opener)
-    pagehandle = urllib2.urlopen(theurl+"/status/status_deviceinfo.htm")
+    pagehandle = urllib2.urlopen(config.MODEM_URL_STATUS_ADDRESS)
 
-    return pagehandle.read()
+    return pagehandle.readlines()
 
-def getModemStatus(page):
+def analizeModemStatus(page):
     """
         This function analises status pages and looks for string that
         says it is connected to line
-        
+
         Input:
         Nothing
-        
+
         Returns:
         True/False
     """
+    import re
+    import config
 
-    stan_lini_idx = page.find("Stan linii")
+    regexp = re.compile(config.MODEM_REGEXP_INTERNET_ACTIVE)
 
-    if page[stan_lini_idx: stan_lini_idx + 200].find("Showtime") != -1:
-        return True
+    for line in page:
+        if regexp.match(line): return True
     else:
         return False
 
+################################################################################
+################################ Web check #####################################
+################################################################################
+def webCheck():
+    """
+        This function opens web page to check if internet is working
+
+        Input:
+        Nothing
+
+        Returns:
+        True/False
+    """
+    import urllib2
+    import config
+    import random
+
+    urls = config.WEBCHECK_PAGE_LIST[:]
+    for idx in range(3):
+        url = random.choice(urls)
+
+        try: page = urllib2.urlopen(url)
+        except BaseException:
+            urls.remove(url)
+        else: return True
+
+    else: return False
+
+################################################################################
+############################## Startup #########################################
+################################################################################
 def startup(root):
     """
         This function performs some start up actions
             - Prepares pid
-            
+
         Input:
         root    - Root path
-        
+
         Returns:
         Nothing
     """
     import os
-    
+
     pid = os.path.join(root,"pid")
     with open(pid,"w") as fp:
         fp.write(str(os.getpid()))
@@ -64,45 +97,45 @@ def internet_check(root):
     """
         This function performs internet check
         Blocking function
-        
+
         Input:
         root        - Path to root folder
-        
+
         Returns:
         Nothing
     """
     import time
     import datetime
     import os
-    
-    logs = os.path.join(root,"logs") 
+
+    logs = os.path.join(root,"logs")
 
     if not os.path.exists(logs): os.mkdir(logs)
 
     while True:
         now = datetime.datetime.now()
 
-        try: modem_status = getModemStatus(getModemPage())
+        try: modem_internet_status = analizeModemStatus(getModemPage())
         except BaseException as error: info = "ERROR: " + str(error)
         else:
-            if modem_status:
-                info = "INTERNET"
-            else:
-                info = "NOP"
+            if modem_internet_status: info = "MODEM CHECK OK"
+            else: info = "MODEM CHECK NOT OK"
+
+            if webCheck(): info += " - WEB CHECK OK"
+            else: info += " - WEB CHECK NOT OK"
 
         with open(os.path.join(logs,now.strftime("%Y-%m-%d.txt")),"a") as fp:
             fp.write(str(now) + " - " + info + "\n")
 
         print str(now) + " - " + info
 
-        time.sleep(60)    
-        
+        time.sleep(60)
+
+################################################################################
+################################ Main ##########################################
+################################################################################
 if __name__ == "__main__":
     import os
     root = os.path.dirname(os.path.abspath(__file__))
     startup(root)
     internet_check(root)
-    
-    
-
-    
