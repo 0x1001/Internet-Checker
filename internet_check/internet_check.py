0bacfd9cf52c15c823dda8ff3a5c7a624d01d49f
Damian Nowok <damian.nowok@gmail.com>
2015-02-08 17:38:51 +0100

    Simplifying internet checker

--- internet_check/internet_check.py
+++ internet_check/internet_check.py
@@ -1,53 +1,10 @@
-################################################################################
-########################## Modem Analyzer ######################################
-################################################################################
-def getModemPage():
-    """
-        This function connects to modem internal web pages and
-        gets status information
+WEBCHECK_PAGE_LIST = ['http://google.com',
+                      'http://www.python.org',
+                      'http://github.com',
+                      'http://www.amazon.com',
+                      'http://www.nasa.gov',
+                      'http://lhcathome.web.cern.ch']
 
-        Input:
-        Nothing
-
-        Returns:
-        Pages string
-    """
-    import urllib2
-    import config
-
-    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-    passman.add_password(None,config.MODEM_URL_ADDRESS,config.MODEM_USERNAME,config.MODEM_PASSWORD)
-    authhandler = urllib2.HTTPBasicAuthHandler(passman)
-    opener = urllib2.build_opener(authhandler)
-    urllib2.install_opener(opener)
-    pagehandle = urllib2.urlopen(config.MODEM_URL_STATUS_ADDRESS)
-
-    return pagehandle.readlines()
-
-def analizeModemStatus(page):
-    """
-        This function analises status pages and looks for string that
-        says it is connected to line
-
-        Input:
-        Nothing
-
-        Returns:
-        True/False
-    """
-    import re
-    import config
-
-    regexp = re.compile(config.MODEM_REGEXP_INTERNET_ACTIVE)
-
-    for line in page:
-        if regexp.match(line): return True
-    else:
-        return False
-
-################################################################################
-################################ Web check #####################################
-################################################################################
 def webCheck():
     """
         This function opens web page to check if internet is working
@@ -59,39 +16,20 @@ def webCheck():
         True/False
     """
     import urllib2
-    import config
     import random
 
-    urls = config.WEBCHECK_PAGE_LIST[:]
+    urls = WEBCHECK_PAGE_LIST[:]
     for idx in range(3):
         url = random.choice(urls)
 
-        try: page = urllib2.urlopen(url)
-        except BaseException:
+        try:
+            page = urllib2.urlopen(url)
+        except Exception:
             urls.remove(url)
-        else: return True
+        else:
+            return True
 
     else: return False
-
-################################################################################
-############################## Startup #########################################
-################################################################################
-def startup(root):
-    """
-        This function performs some start up actions
-            - Prepares pid
-
-        Input:
-        root    - Root path
-
-        Returns:
-        Nothing
-    """
-    import os
-
-    pid = os.path.join(root,"pid")
-    with open(pid,"w") as fp:
-        fp.write(str(os.getpid()))
 
 def internet_check(root):
     """
@@ -110,19 +48,16 @@ def internet_check(root):
 
     logs = os.path.join(root,"logs")
 
-    if not os.path.exists(logs): os.mkdir(logs)
+    if not os.path.exists(logs):
+        os.mkdir(logs)
 
     while True:
         now = datetime.datetime.now()
 
-        try: modem_internet_status = analizeModemStatus(getModemPage())
-        except BaseException as error: info = "ERROR: " + str(error)
+        if webCheck():
+            info = "WEB CHECK OK"
         else:
-            if modem_internet_status: info = "MODEM CHECK OK"
-            else: info = "MODEM CHECK NOT OK"
-
-            if webCheck(): info += " - WEB CHECK OK"
-            else: info += " - WEB CHECK NOT OK"
+            info = "WEB CHECK NOT OK"
 
         with open(os.path.join(logs,now.strftime("%Y-%m-%d.txt")),"a") as fp:
             fp.write(str(now) + " - " + info + "\n")
@@ -137,5 +72,4 @@ def internet_check(root):
 if __name__ == "__main__":
     import os
     root = os.path.dirname(os.path.abspath(__file__))
-    startup(root)
     internet_check(root)
